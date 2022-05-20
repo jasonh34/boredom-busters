f132c9c772cbc6ba7626f74b02847b5dbd221466
jasonh34 <jasonh34.jh@gmail.com>
2022-05-20 10:13:55 -0400

    created main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#TODO: Everything
+
+#import modules
+import random
+import time
+
+#start game
+def start():
+    print("Welcome to Boredom Busters! Presenting text-based Python minigames!")
+    time.sleep(1)
+    input("Press enter to start!")
+
+    #Loading "animation"
+    for i in range(3):
+        print(".",end="")
+        time.sleep(1)
+
+if __name__ == "__main__":
+    start()
+    print("Done!")
+    
+    print("WIP")
+
+    
